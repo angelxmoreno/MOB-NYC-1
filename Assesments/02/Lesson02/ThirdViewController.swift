e7d3395704a268ed6a7dfd5f4a2a48bf414883b7
Angel S. Moreno <angelxmoreno@gmail.com>
2015-01-12 18:05:17 -0500

    Finished HW#2.3

--- Assesments/02/Lesson02/ThirdViewController.swift
+++ Assesments/02/Lesson02/ThirdViewController.swift
@@ -13,4 +13,25 @@ class ThirdViewController: UIViewController {
     TODO six: Hook up the number input text field, button and text label to this class. When the button is pressed, a message should be printed to the label indicating whether the number is even.
 
 */
+    @IBOutlet weak var numInput: UITextField!
+    
+    @IBOutlet weak var numLabel: UILabel!
+    
+    @IBOutlet weak var calBtn: UIButton!
+    
+    @IBAction func calIsEven(sender: AnyObject) {
+        if let num = numInput.text.toInt() {
+            numLabel.text = "\(num) is \(isEven(num))"
+        } else {
+            numLabel.text = "Please enter a number"
+        }
+    }
+    
+    func isEven(num : Int) -> String {
+        if num % 2 == 0 {
+            return "Even"
+        } else {
+            return "ODD"
+        }
+    }
 }
