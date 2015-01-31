ad217cc03dd09d3d1d3ada3a196ca85415d35793
Angel S. Moreno <angelxmoreno@gmail.com>
2015-01-31 15:13:04 -0500

    Completed HW# 4

--- Assesments/04/Lesson04/ArrayViewController.swift
+++ Assesments/04/Lesson04/ArrayViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
-class ArrayViewController: UIViewController {
-
+class ArrayViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate  {
+    let tableView: UITableView = UITableView()
+    let textField: UITextField = UITextField()
+    
+    var items: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,6 +21,52 @@ class ArrayViewController: UIViewController {
         TODO two: Make this class a UITableViewDelegate and UITableViewDataSource that supply the above table view with cells. These cells should correspond to the text entered into the text box. E.g. If the text "one", then "two", then "three" was entered into the text box, there should be three cells in this table view that contain those strings in order.
         
         */
-    }
-}
+        
+        //create tableView programatically
+        tableView.frame = CGRectMake(0, 125, self.view.frame.size.width, self.view.frame.size.height);
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        //create textField programatically
+        textField.frame = CGRectMake(0, 75, self.view.frame.size.width, 50);
+        textField.borderStyle = UITextBorderStyle.Line
+//        textField.backgroundColor = UIColor.redColor()
+        textField.placeholder = "Add Stuff"
+        textField.delegate = self
 
+        self.view.addSubview(textField)
+        self.view.addSubview(tableView)
+
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("You selected cell #\(indexPath.row)!")
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField.text != "" {
+            items.append(textField.text)
+            tableView.reloadData()
+            textField.text = ""
+        }
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
--- Assesments/04/Lesson04/MapViewController.swift
+++ Assesments/04/Lesson04/MapViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate  {
+    let tableView: UITableView = UITableView()
+    let textField1: UITextField = UITextField()
+    let textField2: UITextField = UITextField()
+    
+    var items: [[String:String]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,5 +23,75 @@ class MapViewController: UIViewController {
         TODO four: Make this class a UITableViewDelegate and UITableViewDataSource that supply this table view with cells that correspond to the values in the dictionary. Each cell should print out a unique pair of key/value that the map contains. When a new key/value is inserted, the table view should display it.
         TODO five: Make the background of the text boxes in this controller BLUE when the keyboard comes up, and RED when it goes down. Start with UIKeyboardWillShowNotification and NSNotificationCenter.
         */
+                
+        //create tableView programatically
+        tableView.frame = CGRectMake(0, 175, self.view.frame.size.width, self.view.frame.size.height);
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        //create textField1 programatically
+        textField1.frame = CGRectMake(0, 75, self.view.frame.size.width, 50);
+        textField1.borderStyle = UITextBorderStyle.Line
+        textField1.placeholder = "Key"
+        
+        textField2.frame = CGRectMake(0, 125, self.view.frame.size.width, 50);
+        textField2.borderStyle = UITextBorderStyle.Line
+        textField2.placeholder = "Value"
+        textField2.delegate = self
+        
+        
+        self.view.addSubview(textField1)
+        self.view.addSubview(textField2)
+        self.view.addSubview(tableView)
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        
+        notificationCenter.addObserver(
+            self,
+            selector: Selector("keyboardAppearedAction"),
+            name: UIKeyboardDidShowNotification,
+            object: nil
+        )
+        
+        notificationCenter.addObserverForName(
+            UIKeyboardDidHideNotification,
+            object: nil,
+            queue: nil,
+            usingBlock: { (notification) -> Void in
+                self.textField1.backgroundColor = UIColor.redColor()
+                self.textField2.backgroundColor = UIColor.redColor()
+            }
+
+        )
+    }
+    
+    func keyboardAppearedAction(){
+        textField1.backgroundColor = UIColor.blueColor()
+        textField2.backgroundColor = UIColor.blueColor()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("cell") as UITableViewCell
+        
+        var dict = items[indexPath.row]
+        if let key = dict.keys.first {
+            cell.textLabel?.text =  "\(key) : \(dict[key]!)"
+        }
+        return cell
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField.text != "" {
+            var dict:[String:String] = [textField1.text : textField2.text]
+            items.append(dict)
+            tableView.reloadData()
+            textField1.text = ""
+            textField2.text = ""
+        }
+        return true
     }
 }
